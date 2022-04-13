00ef38cafa39699e76c2e701ed12afb272df2176
Alexander735 <happylesa@icloud.com>
2022-04-13 23:15:42 +0300

    Old version reborn (2)

--- src/CommentsAnalyzerTask/KeywordAnalyzer.java
+++ src/CommentsAnalyzerTask/KeywordAnalyzer.java
@@ -1,8 +1,8 @@
 package CommentsAnalyzerTask;
-
 import java.util.Arrays;
 
 public abstract class KeywordAnalyzer implements TextAnalyzer {
+    private String [] keywords;
 
     public abstract Label processText(String text);
 
